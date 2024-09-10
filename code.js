5686ac7b4df133be744a209fd5b335c474b69a23
Maxie <144727967+Maxie-M@users.noreply.github.com>
2024-09-10 11:16:27 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
-function binarySearch(list, element) {
+//Resource(s): https://medium.com/@jeffrey.allen.lewis/javascript-algorithms-explained-binary-search-25064b896470
+//code.js 
+//Maxie Machado 
+//Algorithms: Binary Search 
+
+function binarySearch(list, element)
+ {
+    let left = 0; 
+    let right = list.length - 1; 
+
+    while (left <= right) 
+    {
+        let middle = Math.floor((left + right) / 2);
+
+        if (list[middle] === element && (middle === 0 || list[middle - 1] !== element))
+        {
+            return middle;
+        }
+        else if (list[middle] < element)
+        {
+            left = middle + 1; 
+        }
+        else 
+        {
+            right = middle - 1;
+        }
+    }
     return -1;
 }
